988fe198cca92caa8cc44110ac1e1975bf987d6b
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-29 14:10:25 +0530

    Add : Creating DataBase

---
+++ AddressBookDB.sql
@@ -0,0 +1,5 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+--Create Address Book Service Database
+create database Address_Book_Service;
+use Address_Book_Service;
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookDatabase
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book Database");
+        }
+    }
+}
